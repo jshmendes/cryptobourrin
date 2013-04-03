395689067e34546938dbd46dde86cc2046a6e7f7
jshmendes@gmail.com <jshmendes@gmail.com@9c67ead6-1a8f-ddab-d02c-297c511ae68e>
2013-04-03 18:12:17 +0000

    adding some basic check of e and N

--- tools.h
+++ tools.h
@@ -1,3 +1,41 @@
+
+
+int isprime(mpz_t x)
+{
+	return mpz_probab_prime_p(x,20);
+}
+
+
+int gcd(mpz_t e, mpz_t N)
+{
+	mpz_t gcd;
+	mpz_init(gcd);	
+
+	mpz_gcd(gcd,e,N);
+	int ret = mpz_cmp_ui(gcd,1)?1:0;
+	
+	mpz_clear(gcd);
+
+	return ret;
+}
+
+
+int isperfectsquare(mpz_t N)
+{
+	mpz_t tmp;
+	mpz_init(tmp);	
+
+	mpz_sqrt(tmp,N);
+	mpz_mul(tmp,tmp,tmp);
+
+	int ret = 	mpz_cmp(tmp,N)?0:1;
+
+	mpz_clear(tmp);
+
+	return ret;
+}
+
+
 mpz_t farr[100000];
 mpz_t karr[10000],darr[10000];
 mpz_t phi;
@@ -69,26 +107,108 @@ void find_k_d(mpz_t a,mpz_t b,int j)
   mpz_clear (x);
 }
 
-/*
-void find_phi(mpz_t e,int n)
-{
-	int i;
-	mpz_t tmp,tmp2;
-	mpz_init(tmp);
-	mpz_init(tmp2);
-   
-	for(i=1;i<=n;i++)
-	{
-		mpz_mul(tmp,e,darr[i]);
-		mpz_sub_ui(tmp,tmp,1);
-		mpz_mod(tmp2,tmp,karr[i]);
-		if(mpz_cmp_ui(tmp2,0)==0)
-		{
-		  mpz_fdiv_q(phi,tmp,karr[i]);
-		  break;
-		}	  
-	}   
-	mpz_clear (tmp);
-	mpz_clear (tmp2);
 
-}*/
+int wiener(mpz_t a, mpz_t b)
+{
+	int j,i,iteration;
+	mpz_t f,tmp,tmpb,r1,r2,x;
+	mpz_t tab[10];
+	mpz_t arr[256];
+	mpz_t k,l,m,t,delta,B;
+	mpz_init(k);mpz_init(l);mpz_init(f);
+	mpz_init(m);mpz_init(t);
+	mpz_init(r1);mpz_init(r2);
+	mpz_init(delta);mpz_init(B);
+	mpz_init(tmp);mpz_init(tmpb);
+
+	gmp_printf("e   is %Zd\n",a);
+	gmp_printf("N   is %Zd\n",b);
+
+	mpz_set(tmp,a);
+	mpz_set(tmpb,b);
+
+	//f = N + 1 - 2sqrt(N)
+	mpz_sqrt(k,tmpb);
+	mpz_mul_ui(l,k,2);
+	mpz_add_ui(k,tmpb,1);
+	mpz_sub(f,k,l);
+
+	j=cfrac(tmp,f);
+   	find_k_d(tmp,tmpb,j);
+   	mpz_set(tmp,a);				
+	mpz_set(tmpb,b);
+
+
+	for(i=0; i < j+1; i++)
+	{
+		if(mpz_cmp_ui(karr[i],0) != 0)		
+		{
+			mpz_mul(k,darr[i],a);
+			mpz_sub_ui(l,k,1);
+			mpz_fdiv_q(t,l,karr[i]);
+
+			//delta	= (b-t+1)^2-4*b
+			mpz_sub(k, tmpb,t);
+			mpz_add_ui(B, k,1);
+			mpz_mul(k,B,B);
+			mpz_mul_ui(l,tmpb, 4);
+			mpz_sub(delta, k,l);
+		
+			if(mpz_cmp_ui(delta, 0) < 0)
+				continue;	
+			mpz_sqrt(k,delta);
+
+			mpz_sub(l,B,k);
+			mpz_fdiv_q_ui(r1,l,2); 
+
+			mpz_add(l,k,B);
+			mpz_fdiv_q_ui(r2,l,2); 
+
+			mpz_mul(k, r1,r2);
+
+			if(mpz_cmp(tmpb,k) ==0)
+			{
+				fprintf(stdout, "Wiener's attack worked !\n");
+				gmp_printf("P   is %Zd \n",r1);
+				gmp_printf("Q   is %Zd\n",r2);
+
+				mpz_sub_ui(r1,r1,1);
+				mpz_sub_ui(r2,r2,1);		
+
+				mpz_mul(r1,r1,r2);
+				gmp_printf("Phi is %Zd\n",r1);
+				mpz_invert(r2,a,r1);
+				gmp_printf("d   is %Zd\n",r2);
+
+				mpz_clear (tmp);
+				mpz_clear (tmpb);
+				mpz_clear (r1);
+				mpz_clear (r2);
+				mpz_clear (k);
+				mpz_clear (l);
+				mpz_clear (f);
+
+				return 0;
+			}
+		}
+	}
+
+	fprintf(stdout, "Wiener's attack failed !\n");
+	mpz_sqrt(tmpb,b);
+	mpz_sqrt(tmp,tmpb);
+
+	mpz_fdiv_q_ui(r1,tmp,3);
+
+	gmp_printf("\td > 1/3*N^{1/4} = %Zd \n",r1); 
+
+	mpz_clear (tmp);
+	mpz_clear (tmpb);
+	mpz_clear (r1);
+	mpz_clear (r2);
+	mpz_clear (k);
+	mpz_clear (l);
+	mpz_clear (f);
+
+	return 1;
+}
+
