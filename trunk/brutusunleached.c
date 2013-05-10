e0b5e7f6f4314f61ee44df28cf09ed8acc3e4132
jshmendes@gmail.com <jshmendes@gmail.com@9c67ead6-1a8f-ddab-d02c-297c511ae68e>
2013-05-10 19:41:55 +0000

    Search for smaller order group

--- trunk/brutusunleached.c
+++ trunk/brutusunleached.c
@@ -12,32 +12,50 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	mpz_t x,N, pow,tmp, min ;
+	mpz_t x,N, pow,tmp, min, minRef ;
 	mpz_init_set_str(x,"2", 16);	
-	mpz_init_set_str(tmp,"100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", 16);	
+	mpz_init_set_str(tmp,"341510400", 10);	
 	mpz_init_set_str(N,argv[1], 16);
 	mpz_init_set_str(pow,"1",10);
-	mpz_init_set_str(min,"10000000000",10);
+	mpz_init_set_str(min,"100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000",16);
+	gmp_randstate_t rstate42;
 	unsigned int i = 1;
 	unsigned int j = 1;
+	unsigned int count = 0;
+
+	mpz_init_set(minRef,N);
 
 	gmp_printf("N = %Z0x\n", N);
-
+	gmp_randinit_default(rstate42);
 	mpz_sub_ui(tmp,tmp,1);
 
-	while(1)
+	mpz_urandomb(tmp, rstate42,2000);
+	gmp_printf("x = %Zd\n", tmp);
+	mpz_init_set(min,tmp);
+	while(count <5)
 	{
-		mpz_add_ui(tmp,tmp,1);
+		count++;
+		mpz_set_str(pow,"0",10);
+		mpz_init_set(tmp,min);
 		gmp_printf("x = %Zd\n", tmp);
-		mpz_mul(x,tmp,tmp);
+		mpz_init_set(min,minRef);
+	
+		mpz_set(x,tmp);
 		i=0;
 		j=0;
 		while(mpz_cmp_ui(x,1) != 0)
 		{
+
 			mpz_add_ui(pow,pow,1);
 			mpz_mul(x,x,tmp);
 			mpz_mod(x,x,N);
 
+			if(mpz_cmp(min,x) == 0)
+			{
+				gmp_printf("looping pow = %Zd\n", pow);
+				break ;
+			}
+			
 			if(mpz_cmp(min,x) > 0)
 			{
 				mpz_set(min, x);
@@ -45,19 +63,14 @@ int main(int argc, char *argv[])
 				gmp_printf("pow = %Zd\n", pow);				
 			}
 
-			if(mpz_cmp(min,x) == 0)
-			{
-				gmp_printf("looping pow = %Zd\n", pow);
-				break ;
-			}
-
 			i++ ;		
 			if (i == 10000000)
 			{
 				gmp_printf("On en est a pow = %Zd\n", pow);
+				//gmp_printf("x = %Zd\n", x);
 				i=0;
 				j++;
-				if(j >= 1000)
+				if(j >= 10)
 				{
 					gmp_printf("Jumping to next...\n");
 					break ;
@@ -65,7 +78,6 @@ int main(int argc, char *argv[])
 			}
 		}
 		gmp_printf("... Ok pour x=%Zd et pow = %Zd\n\n", tmp, pow);
-
 	}
 	return 0;
 }
